cefaf13653df47faf96eacd3413dcaf493d8947e
Erick Dario Garcia Suarez <erick_dario2492@hotmail.com>
2015-03-07 01:09:35 -0600

    pauses video when not target on camera

--- Assets/Qualcomm Augmented Reality/Scripts/DefaultTrackableEventHandler.cs
+++ Assets/Qualcomm Augmented Reality/Scripts/DefaultTrackableEventHandler.cs
@@ -15,6 +15,16 @@ public class DefaultTrackableEventHandler : MonoBehaviour,
     #region PRIVATE_MEMBER_VARIABLES
  
     private TrackableBehaviour mTrackableBehaviour;
+
+	private bool mHasBeenFound = false;
+	private bool mLostTracking;
+	private float mSecondsSinceLost;
+	private float distanceToCamera;
+	
+	private float mVideoCurrentPosition;
+	private float mCurrentVolume;
+	
+	private Transform mSphere;
     
     #endregion // PRIVATE_MEMBER_VARIABLES
 
@@ -30,6 +40,43 @@ public class DefaultTrackableEventHandler : MonoBehaviour,
             mTrackableBehaviour.RegisterTrackableEventHandler(this);
         }
     }
+
+	void Update()
+	{
+		//for testing audio levels while in editor      
+		//distanceToCamera = Vector3.Distance(Camera.main.transform.position, transform.root.position);
+		//Debug.Log(Mathf.Clamp01(1.0f-distanceToCamera*0.01f));
+
+		if (mHasBeenFound) {
+			VideoPlaybackBehaviour video = GetComponentInChildren<VideoPlaybackBehaviour>();
+			mCurrentVolume = 1.0f-(Mathf.Clamp01(distanceToCamera*0.006f)*0.5f);
+			video.VideoPlayer.SetVolume(mCurrentVolume);
+		}
+
+		// Pause the video if tracking is lost for more than n seconds
+		if (mHasBeenFound && mLostTracking) {
+				VideoPlaybackBehaviour video = GetComponentInChildren<VideoPlaybackBehaviour> ();
+				if (video != null &&
+					video.CurrentState == VideoPlayerHelper.MediaState.PLAYING) {
+					//fade out volume from current if marker is lost
+					//Debug.Log(mCurrentVolume - mSecondsSinceLost);
+					video.VideoPlayer.SetVolume (Mathf.Clamp01 (mCurrentVolume - mSecondsSinceLost));
+				}
+				//n.0f is number of seconds before playback stops when marker is lost
+				if (mSecondsSinceLost > 1.0f) {   
+					if (video != null &&
+						video.CurrentState == VideoPlayerHelper.MediaState.PLAYING) {
+						//get last position so it can resume after video is unloaded and reloaded.
+						mVideoCurrentPosition = video.VideoPlayer.GetCurrentPosition ();
+						video.VideoPlayer.Pause ();
+	
+		
+					}
+					mLostTracking = false;
+				}
+			mSecondsSinceLost += Time.deltaTime;
+		}
+	}
 
     #endregion // UNTIY_MONOBEHAVIOUR_METHODS
 
@@ -82,6 +129,9 @@ public class DefaultTrackableEventHandler : MonoBehaviour,
         }
 
         Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+
+		mHasBeenFound = true;
+		mLostTracking = false;
     }
 
 
@@ -103,6 +153,9 @@ public class DefaultTrackableEventHandler : MonoBehaviour,
         }
 
         Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+
+		mLostTracking = true;
+		mSecondsSinceLost = 0;
     }
 
     #endregion // PRIVATE_METHODS
